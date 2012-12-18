b887f17a1637cf31ea214c167a86d16be1d32667
Fujii Takuya <takuya327@gmail.com>
2012-12-18 19:58:30 +0900

    Minor change

--- lib/r_decorator/engine.rb
+++ lib/r_decorator/engine.rb
@@ -1,7 +1,7 @@
 module RDecorator
   class Engine < ::Rails::Engine
     config.after_initialize do
-      Dir["#{Rails.root}/app/decorators/**/*.rb"].each {|f| require_dependency f}
+      Dir[ File.join( Rails.root, "app", "decorators", "**", "*.rb" ) ].each {|f| require_dependency f}
     end
   end
 end
